c08dc23fa2208ad9156d4be9cbd84acddba30dcb
Alex Bilson <acbilson@gmail.com>
2021-12-13 15:55:45 -0600

    splits project into client/server images

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,11 +6,17 @@ ENVIRONMENT=$1
 case $ENVIRONMENT in
 
 dev)
-  echo "builds development image..."
-  docker build -f Dockerfile \
+  echo "builds development client image..."
+  docker build -f client/Dockerfile \
     --target=dev \
     --build-arg EXPOSED_PORT=${EXPOSED_PORT} \
-    -t acbilson/${IMAGE_NAME}-dev:6.0 .
+    -t acbilson/${IMAGE_NAME}-client-dev:6.0 .
+
+  echo "builds development server image..."
+  docker build -f server/Dockerfile \
+    --target=dev \
+    --build-arg EXPOSED_PORT=5000 \
+    -t acbilson/${IMAGE_NAME}-server-dev:6.0 .
 ;;
 
 prod)
